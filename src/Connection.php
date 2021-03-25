7284b5580886828fc92a73971934a5f22d845127
Mashin Oleg <o.y.mashin@gmail.com>
2021-03-25 23:04:30 +0300

    init

---
+++ src/Connection.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace LoyalmeSdk;
+
+class Connection implements ConnectionInterface
+{
+    private $_url;
+    private $_path = '';
+    private $_token;
+    private $_brandId;
+    private $_pointId;
+    private $_personId;
+
+    public function __construct(
+        string $token,
+        string $url,
+        int $brandId,
+        int $pointId,
+        int $personId
+    ) {
+        $this->_token = $token;
+        $this->_url = $url;
+        $this->_brandId = $brandId;
+        $this->_pointId = $pointId;
+        $this->_personId = $personId;
+    }
+
+    private function _sendRequest(string $method, array $params)
+    {
+        $url = $this->_url . '/' . $this->getPath();
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [sprintf('Authorization: Bearer %s', $this->_token), 'accept: application/json', 'Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        if (in_array($method, [self::METHOD_POST, self::METHOD_PUT])) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+        }
+        $output = curl_exec($ch);
+        curl_close($ch);
+        $output = json_decode($output);
+
+        if (isset($output->data)) {
+            foreach ($output->data as $key => $itemData) {
+                if (is_numeric($key)) {
+                    foreach ($itemData as $itemField => $itemValue) {
+                        $this->{$itemField} = $itemValue;
+                    }
+                } else {
+                    $this->{$key} = $itemData;
+                }
+            }
+            $result = $this;
+        } else {
+            $result = $output;
+        }
+        return $result;
+    }
+
+    protected function _setPath(string $path)
+    {
+        $this->_path = $path;
+        return $this;
+    }
+
+    public function sendGetRequest(string $action, array $params)
+    {
+        $this->_setPath($action . '?' . http_build_query($params, '', '&'));
+
+        return $this->_sendRequest(self::METHOD_GET, $params);
+    }
+
+    public function sendPostRequest(string $action, array $params)
+    {
+        $this->_setPath($action);
+
+        return $this->_sendRequest(self::METHOD_POST, $params);
+    }
+
+    public function sendPutRequest(string $action, array $params)
+    {
+        $this->_setPath($action);
+
+        return $this->_sendRequest(self::METHOD_PUT, $params);
+    }
+
+    public function sendDeleteRequest(string $action, array $params)
+    {
+        $this->_setPath($action);
+
+        return $this->_sendRequest(self::METHOD_PUT, $params);
+    }
+
+    public function getPath(): string
+    {
+        return $this->_path;
+    }
+}
---
+++ src/ConnectionInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LoyalmeSdk;
+
+interface ConnectionInterface
+{
+    const METHOD_GET = 'GET';
+    const METHOD_POST = 'POST';
+    const METHOD_PUT = 'PUT';
+    const METHOD_DELETE = 'DELETE';
+
+    public function sendGetRequest(string $action, array $params);
+    public function sendPostRequest(string $action, array $params);
+    public function sendPutRequest(string $action, array $params);
+    public function sendDeleteRequest(string $action, array $params);
+}
