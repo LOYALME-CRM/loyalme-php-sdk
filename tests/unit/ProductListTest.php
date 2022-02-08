3ef0954952aead149f7cf563f4ffcd40f52beaf8
Олег <olejan1991@gmail.com>
2022-02-08 11:37:24 +0300

    pd-798. Refinement of the ProductList class (#30)
    * pd-798. finalized ProductList class for changing of product list content
    
    * pd-798. Added method for updating and clearing of product list in ProductList class
    
    Co-authored-by: Mashin Oleg <o.y.mashin@gmail.com>

--- tests/unit/ProductListTest.php
+++ tests/unit/ProductListTest.php
@@ -1,9 +1,15 @@
 <?php
 
+use Faker\Factory;
 use Codeception\Test\Unit;
+use LoyalmeCRM\LoyalmePhpSdk\Client;
+use LoyalmeCRM\LoyalmePhpSdk\Product;
 use LoyalmeCRM\LoyalmePhpSdk\Connection;
-use LoyalmeCRM\LoyalmePhpSdk\Exceptions\ProductListException;
 use LoyalmeCRM\LoyalmePhpSdk\ProductList;
+use LoyalmeCRM\LoyalmePhpSdk\Interfaces\ClientInterface;
+use LoyalmeCRM\LoyalmePhpSdk\Interfaces\ProductInterface;
+use LoyalmeCRM\LoyalmePhpSdk\Exceptions\ProductListException;
+use LoyalmeCRM\LoyalmePhpSdk\Interfaces\ProductListInterface;
 
 class ProductListTest extends Unit
 {
@@ -85,9 +91,188 @@ class ProductListTest extends Unit
      */
     public function systemNameProvider(): Generator
     {
-        $faker = Faker\Factory::create();
-        for ($i = 0; $i < 10;  $i++) {
-            yield [$faker->firstName, $faker->lastName];
+        $time = time();
+        $faker = Factory::create();
+        for ($i = 0; $i < rand(1, 10);  $i++) {
+            yield [$faker->firstName . $time, $faker->lastName . $time];
+        }
+    }
+
+    private function _startTestUpdatingContentOfProductList(ProductListInterface $productList, ClientInterface $client = null, ProductInterface $relatedProduct = null)
+    {
+        $extItemId = rand(100000, 999999);
+        $barcode = md5(date('Ymdhms') . rand(10000, 99999));
+        $title = 'productTestSDK-' . rand(1000, 9999);
+        $price = rand(1000, 9999);
+        $productObject = new Product($this->_connection);
+        $product1 = $productObject->get(
+            $extItemId,
+            $barcode,
+            $title,
+            $price
+        );
+        $this->assertTrue($product1 instanceof Product);
+
+        $extItemId = rand(100000, 999999);
+        $barcode = md5(date('Ymdhms') . rand(10000, 99999));
+        $title = 'productTestSDK-' . rand(1000, 9999);
+        $price = rand(1000, 9999);
+        $productObject2 = new Product($this->_connection);
+        $product2 = $productObject2->get(
+            $extItemId,
+            $barcode,
+            $title,
+            $price
+        );
+        $this->assertTrue($product2 instanceof Product);
+
+        $products = [$product1->id => $product1, $product2->id => $product2];
+        $content = $productList->updateContent($productList, $products, $client, $relatedProduct);
+        $this->_checkEqualsOfContent($content, $products, $client, $relatedProduct);
+
+        $extItemId = rand(100000, 999999);
+        $barcode = md5(date('Ymdhms') . rand(10000, 99999));
+        $title = 'productTestSDK-' . rand(1000, 9999);
+        $price = rand(1000, 9999);
+        $productObject3 = new Product($this->_connection);
+        $product3 = $productObject3->get(
+            $extItemId,
+            $barcode,
+            $title,
+            $price
+        );
+        $this->assertTrue($product2 instanceof Product);
+        $product1->quantity = 5;
+        $products[$product1->id] = $product1;
+        $product2->price = 500;
+        $products[$product2->id] = $product2;
+        $products[$product3->id] = $product3;
+
+        $content = $productList->updateContent($productList, $products, $client, $relatedProduct);
+        $this->_checkEqualsOfContent($content, $products, $client, $relatedProduct);
+    }
+
+    /**
+     * @param array $content
+     * @param array $products
+     * @return void
+     */
+    private function _checkEqualsOfContent(array $content, array $products, ClientInterface $client = null, ProductInterface $relatedProduct = null): void
+    {
+        $this->assertEquals(count($products), count($content));
+
+        foreach ($content as $productFromContent) {
+            $product = $products[$productFromContent['product_id']];
+            $this->assertEquals($productFromContent['client_id'], $client->id ?? null);
+            $this->assertEquals($productFromContent['related_product_id'], $relatedProduct->id ?? null);
+            $this->assertEquals($productFromContent['price_per_item'], $product->price);
+            $this->assertEquals($productFromContent['quantity'], $product->quantity ?? 1);
+        }
+    }
+
+    private function _creatingTestData()
+    {
+        $time = time();
+        $faker = Factory::create();
+
+        $productListObject = new ProductList($this->_connection);
+        $productList = $productListObject->get($faker->firstName . $time, $faker->lastName . $time);
+
+        $clientRest = new Client($this->_connection);
+        $externalId = rand(100000, 999999);
+        $client = $clientRest->get(
+            $externalId,
+            null,
+            $faker->firstName
+        );
+        $this->assertTrue($client instanceof Client);
+
+        $externalId = rand(100000, 999999);
+        $price = rand(1000, 9999);
+        $productObject = new Product($this->_connection);
+        $relatedProduct = $productObject->get(
+            $externalId,
+            null,
+            $faker->title,
+            $price
+        );
+        $this->assertTrue($relatedProduct instanceof Product);
+
+        return [
+            'productList' => $productList,
+            'client' => $client,
+            'relatedProduct' => $relatedProduct,
+        ];
+    }
+
+    /**
+     * @param ProductListInterface $productList
+     * @param ClientInterface|null $client
+     * @param ProductInterface|null $relatedProduct
+     * @return void
+     */
+    public function _startTestClearingContentOfProductList(ProductListInterface $productList, ?ClientInterface $client = null, ?ProductInterface $relatedProduct = null): void
+    {
+        $extItemId = rand(100000, 999999);
+        $barcode = md5(date('Ymdhms') . rand(10000, 99999));
+        $title = 'productTestSDK-' . rand(1000, 9999);
+        $price = rand(1000, 9999);
+        $productObject = new Product($this->_connection);
+        $product1 = $productObject->get(
+            $extItemId,
+            $barcode,
+            $title,
+            $price
+        );
+        $this->assertTrue($product1 instanceof Product);
+
+        $extItemId = rand(100000, 999999);
+        $barcode = md5(date('Ymdhms') . rand(10000, 99999));
+        $title = 'productTestSDK-' . rand(1000, 9999);
+        $price = rand(1000, 9999);
+        $productObject2 = new Product($this->_connection);
+        $product2 = $productObject2->get(
+            $extItemId,
+            $barcode,
+            $title,
+            $price
+        );
+        $this->assertTrue($product2 instanceof Product);
+
+        $products = [$product1->id => $product1, $product2->id => $product2];
+        $content = $productList->updateContent($productList, $products, $client, $relatedProduct);
+        $this->_checkEqualsOfContent($content, $products, $client, $relatedProduct);
+
+        $this->assertTrue($productList->clear($productList));
+    }
+
+    public function testUpdatingContentOfProductList()
+    {
+        $res = $this->_creatingTestData();
+        $productListTests = [
+            [$res['productList']],
+            [$res['productList'], $res['client']],
+            [$res['productList'], $res['client'], $res['relatedProduct']],
+            [$res['productList'], null, $res['relatedProduct']],
+        ];
+
+        foreach ($productListTests as $productListTest) {
+            $this->_startTestUpdatingContentOfProductList($productListTest[0], $productListTest[1] ?? null, $productListTest[2] ?? null);
+        }
+    }
+
+    public function testClearingOfProductList()
+    {
+        $res = $this->_creatingTestData();
+        $productListTests = [
+            [$res['productList']],
+            [$res['productList'], $res['client']],
+            [$res['productList'], $res['client'], $res['relatedProduct']],
+            [$res['productList'], null, $res['relatedProduct']],
+        ];
+
+        foreach ($productListTests as $productListTest) {
+            $this->_startTestClearingContentOfProductList($productListTest[0], $productListTest[1] ?? null, $productListTest[2] ?? null);
         }
     }
 }
